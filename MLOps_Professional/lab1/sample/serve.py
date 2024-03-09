93ec7f72d5bb3b350a601f5e6c9c6a5f25da9f3d
Alex Beckwith <witzbeck@gmail.com>
2024-03-09 20:14:59 +0000

    finish lab1

--- MLOps_Professional/lab1/sample/serve.py
+++ MLOps_Professional/lab1/sample/serve.py
@@ -25,11 +25,19 @@ async def ping():
     """
     return {"message":"Server is Running"}
 
+
 @app.post("/maintenance")
 async def predict(payload:MaintenancePayload):
     
-    maintenance_result = test_maintenance(payload.temperature)
+    maintenance_result = test_maintenance(payload.temperature, payload.hydraulic_pressure)
     return {"msg": "Completed Analysis", "Maintenance Status": maintenance_result}
+
+
+@app.post("/supportbot")
+async def predict(msg: str):
+    if msg == "help":
+        return {"msg": "bring the harvester in for maintenance"}
+
 
 if __name__ == "__main__":
     uvicorn.run("serve:app", host="0.0.0.0", port=5000, log_level="info")
