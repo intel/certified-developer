cf08298f4219d2be1f5f4fd87c96bbada59465a6
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2024-04-06 03:17:25 +0000

    refine inits

--- MLOps_Professional/lab5/mlo_lab5/__init__.py
+++ MLOps_Professional/lab5/mlo_lab5/__init__.py
@@ -1,30 +1,28 @@
-
-from os import getenv, environ
+from logging import basicConfig, debug
+from os import environ
 from pathlib import Path
 from sys import path
 
-here = Path(__file__).parent
+import logging
+
+debug(here := Path(__file__).parent)
 # Add parent directory to path
 path.append(str(Path.cwd().parent))
 
 
-dotenv_path = here / ".env"
-if dotenv_path.exists():
-    lines = [
-        x.split("#")[0].strip()
-        if "#" in x
-        else x
-        for x in
-        dotenv_path.read_text().splitlines()
-    ]
-    pairs = [
-        x.split("=")
-        for x in lines
-        if x
-    ]
-    for key, value in pairs:
-        if key not in getenv():
-            print(f"Setting {key} to {value}")
-            getenv()[key] = value
+if not (dotenv_path := here.parent / ".env").exists():
+    raise FileNotFoundError(f"Could not find {dotenv_path}")
 
-print(getenv("OMP_NUM_THREADS"))
+lines = [
+    x.split("#")[0].strip() if "#" in x else x
+    for x in dotenv_path.read_text().splitlines()
+]
+pairs = [x.split("=") for x in lines if x]
+for key, value in pairs:
+    if key not in environ:
+        debug(f"Setting {key} to {value}")
+        environ[key] = value
+    else:
+        debug(f"{key} already set to {environ[key]}")
+
+basicConfig(level=getattr(logging, environ.get("LOG_LEVEL", "INFO")))
--- MLOps_Professional/lab5/mlo_lab5/main.py
+++ MLOps_Professional/lab5/mlo_lab5/main.py
@@ -1,11 +1,60 @@
+""" Experiment Logger """
+
 from dataclasses import dataclass, field
-from json import loads
+from json import JSONDecodeError, loads
+from logging import getLogger, warning
+from pathlib import Path
 
-from __init__ import 
+from __init__ import here
 
-@dataclass(frozen=True)
-class ExperimentData:
+logger = getLogger(__name__)
 
+@dataclass
+class ExperimentObject:
+    name: str = field()
+    description: str = field(init=False)
+    data: dict = field(init=False)
+    path: Path = field(init=False)
+    
+    @staticmethod
+    def get_path(clsname: str) -> Path:
+        path = here.parent / f"data/{clsname}.json"
+        if not path.exists():
+            raise FileNotFoundError(f"Could not find {path}")
+        return path
+
+    @staticmethod
+    def get_data(path: Path) -> dict:
+        try:
+            data = loads(path.read_text())
+        except JSONDecodeError:
+            warning(f"Could not load data from {path}")
+        return data
+        
+    def __post_init__(self):
+        self.path = ExperimentObject.get_path(self.__class__.__name__)
+        self.data = ExperimentObject.get_data(self.path)
+
+
+@dataclass
+class ExperimentRecord(ExperimentObject):
+    """Experiment record"""
+    description: str = "Record of the experiment"
 
     def __post_init__(self):
-        self.__dict__ = loads()
+        super().__post_init__()
+        self.data["name"] = self.data["name"].replace("{}", self.name)
+
+
+@dataclass
+class ExperimentParameters(ExperimentObject):
+    """Environment variables for the experiment"""
+    name: str = field(default="parameters")
+    description: str = "Parameters for the experiment"
+
+
+if __name__ == '__main__':
+    record = ExperimentRecord(name="Example")
+    print(record.data)
+    parameters = ExperimentParameters()
+    print(parameters.data)
